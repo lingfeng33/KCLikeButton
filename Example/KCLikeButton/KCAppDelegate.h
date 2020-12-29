0836a38c4a32410e333382e1ce6d46883821ccf6
lingfeng33 <237946843>
2020-12-29 16:36:28 +0800

    Initial commit

---
+++ Example/KCLikeButton/KCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KCAppDelegate.h
+//  KCLikeButton
+//
+//  Created by lingfeng33 on 12/29/2020.
+//  Copyright (c) 2020 lingfeng33. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KCLikeButton/KCViewController.m
@@ -0,0 +1,29 @@
+//
+//  KCViewController.m
+//  KCLikeButton
+//
+//  Created by lingfeng33 on 12/29/2020.
+//  Copyright (c) 2020 lingfeng33. All rights reserved.
+//
+
+#import "KCViewController.h"
+
+@interface KCViewController ()
+
+@end
+
+@implementation KCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
